9c60aa41f9a743ceccafba24c3ed129029c7200e
AshoShibu <ashoshibu@gmail.com>
2025-04-20 19:00:21 +0530

    Update Division.java

--- Division.java
+++ Division.java
@@ -1,1 +1,10 @@
-
+// Class to perform division operation.
+public class Division {
+    public static double divide(double dividend, double divisor) {
+        // Check for division by zero
+        if (divisor == 0) {
+            throw new ArithmeticException("Division by zero is not allowed.");
+        }
+        return dividend / divisor;
+    }
+}
