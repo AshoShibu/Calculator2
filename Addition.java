e3a1f12c7c35cd25335c28af143c93e96fb38508
AshoShibu <ashoshibu@gmail.com>
2025-04-20 18:59:28 +0530

    Update Addition.java

--- Addition.java
+++ Addition.java
@@ -1,1 +1,7 @@
-
+// Class to perform addition operation.
+public class Addition {
+    // Method to return the sum of two numbers.
+    public static double add(double a, double b) {
+        return a + b;
+    }
+}
