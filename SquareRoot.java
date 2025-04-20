a74c416f5a3c027e4ee88a8bd3a7d468adcf9e66
AshoShibu <ashoshibu@gmail.com>
2025-04-20 19:01:21 +0530

    Update SquareRoot.java

--- SquareRoot.java
+++ SquareRoot.java
@@ -1,1 +1,10 @@
-
+// Class to calculate the square root of a number.
+public class SquareRoot {
+    public static double squareRoot(double a) {
+        // Check for negative number input
+        if (a < 0) {
+            throw new ArithmeticException("Cannot calculate square root of a negative number.");
+        }
+        return Math.sqrt(a);
+    }
+}
